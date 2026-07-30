cce3c7607704ed610e563bdcbf59e96090497778
J-Banz <jethyconb@gmail.com>
2026-07-31 04:13:06 +0800

    Update Kulot Friends

---
+++ app/src/main/java/com/onrender/kulot_friends_chat/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.onrender.kulot_friends_chat.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
